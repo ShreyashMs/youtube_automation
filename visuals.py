923011261e478b9cf7df53700abd36ba4cc79eb0
ShreyashMs <shreyashmahalle010@gmail.com>
2026-06-04 00:36:15 +0530

    deleted visuals and topics in cleanup process

--- visuals.py
+++
@@ -1,9 +0,0 @@
-VISUAL_MAP = {
-    "कर्ण": "ancient warrior",
-    "अश्वत्थामा": "dark warrior",
-    "रावण": "fire king cinematic",
-    "कृष्ण": "krishna temple",
-    "रामसेतु": "ancient ocean bridge",
-    "महाभारत": "epic battlefield",
-    "हनुमान": "monkey god statue",
-}
